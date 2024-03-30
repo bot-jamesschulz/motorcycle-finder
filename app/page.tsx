37ae899560d372193cca3accb25449dba88c3eae
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2024-03-30 11:27:18 -0700

    Add dealer location/distance and update zipcode-latlong data

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,19 @@
 
 import { FormSchemaType, Search } from '@/components/Search'
 import { useState } from 'react'
-import { Results, NoResults, ListingType } from '@/components/Results'
+import { Results, NoResults } from '@/components/Results'
 import { LocationMiss } from '@/components/LocationMiss'
 import { ThemeProvider } from '@/components/theme-provider'
 import { ModeToggle } from '@/components/ui/mode-toggle'
 import { LoaderCircle } from 'lucide-react'
+import type { Database } from '@/lib/database.types';
 import Moto from '@/public/Moto'
 
+type ListingsRow = Database['public']['Functions']['keyword_proximity_search']['Returns'];
+
 export default function Home() {
-  const [listings, setListings] = useState<ListingType[]>([])
+  
+  const [listings, setListings] = useState<ListingsRow[]>([])
   const [loadingState, setLoadingState] = useState<null | string>(null) // If results are found or not
 
   const searchHandler = async (values: FormSchemaType) => {
@@ -56,8 +60,8 @@ export default function Home() {
                 <div className={`absolute left-1/2 transform -translate-x-1/2 pointer-events-none ${loadingState === 'loading' ? 'opacity-1':  'opacity-0'}`}>
                   <div className='relative mt-10 min-w-56 animate-bounce'>
                   <Moto />
-                  <LoaderCircle className="absolute top-[3rem] right-[8.3rem] animate-spin duration-2000" strokeWidth={3} size={65} />
-                  <LoaderCircle className="absolute top-[3.4rem] left-[9.4rem] animate-spin duration-1750" strokeWidth={2.5} size={58} />
+                  <LoaderCircle className="absolute top-[3rem] right-[8.3rem] animate-spin duration-2000" strokeWidth={2.5} size={65} />
+                  <LoaderCircle className="absolute top-[3.4rem] left-[9.4rem] animate-spin duration-1750" strokeWidth={2} size={58} />
                   </div>
                 </div>
                 </div>
@@ -65,7 +69,6 @@ export default function Home() {
               {loadingState === 'loaded' && <Results listings={listings} />}
               {loadingState === 'no results' && <NoResults />}
               {loadingState === 'location not found' && <LocationMiss />}
-            
           </div>
         </div>
       </ThemeProvider>
--- components/Results.tsx
+++ components/Results.tsx
@@ -8,44 +8,68 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import type { Database } from '@/lib/database.types';
+import { MapPin, Gauge } from 'lucide-react';
 
-export type ListingType = {
-    make: string,
-    model: string,
-    year: number,
-    condition: string | null,
-    detailsurl: string,
-    imgsrc: string
-}
+type ListingsRow = Database['public']['Functions']['keyword_proximity_search']['Returns'];
 
-export function Results({ listings }: { listings: ListingType[] }) {
+export function Results({ listings }: { listings: ListingsRow[] }) {
 
     console.log('listings', listings)
-    console.log('listings length', listings.length)
-    console.log('listings type', typeof listings)
 
     return (
     <div className='flex flex-wrap justify-center items-center gap-5 max-w-screen-xl'>
         {listings.length > 0 ? 
-            listings?.slice(0,50)?.map(({ make, model, year, condition, detailsurl, imgsrc }) => (
-                <a key={detailsurl} href={detailsurl} rel='external' target='_blank'>
-                <Card className='flex flex-col justify-start items-center w-[250px] h-[400px] overflow-hidden sm:flex-row sm:w-[600px] sm:h-[200px] sm:items-start' >
-                    <div className='w-[250px] h-[200px]'>
-                    <Image 
-                        src={imgsrc}
-                        width={1000}
-                        height={1000}
-                        alt=''
-                        className='object-cover w-full h-full rounded'
-                    />    
-                    </div>            
-                    <CardHeader className='w-full'>
-                        <CardTitle className=''>{make.toUpperCase()} {model.toUpperCase()} </CardTitle>
-                        <CardDescription className='flex justify-between'><span>{year}</span><span>{condition}</span></CardDescription>
-                    </CardHeader>             
-                </Card>
-                </a>
-            ))
+            listings?.slice(0,50)?.map(({ make, model, year, price, mileage, condition, detailsUrl, imgSrc, distance, city, dealerName}) => {
+                const formattedPrice = price?.toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                    minimumFractionDigits: 0
+                })
+                const formattedMileage = mileage?.toLocaleString()
+                const conditionFinal = condition &&
+                    condition.charAt(0).toUpperCase() + condition.slice(1)
+                const milesConversionFactor = 0.0006213712;
+                const distanceInMiles = distance * milesConversionFactor;
+                const dealerNameTrunc = dealerName.slice(0,15) + '...'
+
+                return (
+                    <a key={detailsUrl} href={detailsUrl} rel='external' target='_blank'>
+                    <Card className='flex flex-col justify-start items-center overflow-hidden sm:flex-row sm:w-[600px] sm:h-[200px] sm:items-start' >
+                        <div className='w-[250px] h-[200px]'>
+                            <Image 
+                                src={imgSrc || ''}
+                                width={1000}
+                                height={1000}
+                                alt=''
+                                className='object-cover w-full h-full rounded'
+                            />    
+                        </div>     
+                        <div className='grow h-full flex flex-col justify-between'>       
+                            <CardHeader className='w-full'>
+                                <CardTitle className='flex justify-between'><span>{make.toUpperCase()} {model?.toUpperCase()}</span><span className='text-xl font-normal leading-none tracking-tight'>{formattedPrice}</span></CardTitle>
+                                <CardDescription className='flex justify-between text-md'><span>{year}</span><span>{conditionFinal}</span></CardDescription>
+                            </CardHeader>   
+                            <CardContent className='flex flex-col gap-2'>
+                                {mileage && conditionFinal === 'Used' &&
+                                    <div className='flex gap-1 text-sm'>
+                                        <Gauge className='text-slate-800' size={20} strokeWidth={2}/>{formattedMileage} miles
+                                    </div>}                  
+                                <div className='flex justify-between text-nowrap text-sm text-slate-500'>
+                                    <div className='flex gap-1'>
+                                        <MapPin className='text-slate-800 self-center' size={20} strokeWidth={1.5}/>
+                                        <div className='flex flex-col'>
+                                            <span>{city},</span>
+                                            <span>{distanceInMiles?.toFixed(2)} miles away</span>
+                                        </div>
+                                    </div>
+                                    <div className='self-end'>{dealerNameTrunc}</div>
+                                </div>
+                            </CardContent> 
+                        </div>         
+                    </Card>
+                    </a>
+                )})
             : null
         }
 
