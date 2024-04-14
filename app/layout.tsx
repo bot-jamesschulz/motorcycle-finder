bf66e57ece9947c682d446dcf3755f5b69aa838c
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2024-04-13 19:48:16 -0700

    fix: footer spacing

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Motorcycle Finder",
+  title: "Motorcycle Finder: New and Used Dealership motorcycles for sale.",
   description: "Find your next motorcycle, with the most comprehensive database of new and used dealership motorcycle listings available.",
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -192,20 +192,18 @@ export default function Home() {
             </div>
           </div> 
         </main>
-        {!loadingState && 
-          <div className='mt-48 mb-12 sm:mt-8 flex-col mx-auto w-2/3 max-w-2xl rounded-xl p-8'>
-           
-          </div>}
+        
+          <div className={`${loadingState ? 'mt-52' : 'mt-24'} mb-12 sm:mt-8 flex-col mx-auto w-2/3 max-w-2xl rounded-xl p-8`}> 
+          </div>
         <footer className='flex flex-col justify-center items-center w-full py-4 bg-slate-200 shadow dark:bg-slate-950 dark:text-slate-50 text-center rounded-t-2xl'>
           <div className='w-3/4 font-light flex flex-col justify-center items-center gap-2'>
             <h2 className="text-lg tracking-tight">
-              <p>Currently only aggregates <span className='font-normal'>California dealerships.</span></p>
+              <p>Currently only aggregates <span className='font-normal'>California dealerships</span> , at the moment.</p>
               
               <p className='mt-2'>Motorcycle Finder offers the most complete dataset of dealership listings available. Listings are sourced directly from each dealer&apos;s website, so it&apos;s not necessary for them to share their inventory in order for their listings to show up.</p>
               
               <Separator className='bg-slate-800 dark:bg-slate-200 w-3/4 mx-auto mt-1'/>
             </h2>
-            
             <p> 
               <span className='flex items-center gap-2 text-sm'><Mail size={17}/> botjameschulz@gmail.com</span>
             </p>
