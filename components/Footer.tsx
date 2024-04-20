34ab36f54782fcb9829c4f166b39aa9682ee31ec
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2024-04-20 01:05:29 -0700

    fix: footer typo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     return (
         <div className='w-3/4 font-light flex flex-col justify-center items-center gap-2'>
             <h2 className="text-lg tracking-tight">
-                <p>Currently only aggregates <span className='font-normal'>California dealerships</span>, at the moment.</p>
+                <p>Currently only aggregates <span className='font-normal'>California dealerships</span>.</p>
                 
                 <p className='mt-2'>Motorcycle Finder offers the most complete dataset of dealership listings available. Listings are sourced directly from each dealer&apos;s website, so it&apos;s not necessary for them to share their inventory in order for their listings to show up.</p>
                 
