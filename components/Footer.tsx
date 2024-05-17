a02d348dac7e8e983153a639b21c845b2e066e9d
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2024-05-16 21:32:13 -0700

    Update footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,10 +5,7 @@ export function Footer() {
     return (
         <div className='w-3/4 font-light flex flex-col justify-center items-center gap-2'>
             <h2 className="text-lg tracking-tight">
-                <p>Currently only aggregates <span className='font-normal'>California dealerships</span>.</p>
-                
-                <p className='mt-2'>Motorcycle Finder offers the most complete dataset of dealership listings available. Listings are sourced directly from each dealer&apos;s website, so it&apos;s not necessary for them to share their inventory in order for their listings to show up.</p>
-                
+                <p>Currently only aggregates <span className='font-normal'>California dealerships</span>.</p>  
                 <Separator className='bg-slate-800 dark:bg-slate-200 w-3/4 mx-auto mt-1'/>
             </h2>
             <p> 
